2633cd35909e670b70d8b357cbbd961bc5b0e4bd
Wayne E. Seguin <wayneeseguin@gmail.com>
2009-05-21 15:34:29 -0400

    Added /page_cache

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,2 +1,13 @@
 class PublicController < ApplicationController
+
+  caches_page :page_cache
+
+  def index
+    render
+  end
+
+  def page_cache
+    render
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => "public"
+  map.root                      :controller => "public", :action => "index"
+  map.page_cache "/page_cache", :controller => "public", :action => "page_cache"
 end
